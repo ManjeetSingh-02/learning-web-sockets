17cab9c6a2581160a104d4e2e22b362e580fd65b
ManjeetSingh-02 <manjeetsingh.wrk@gmail.com>
2026-03-26 02:59:01 +0530

    added redis pub-sub with socket.io for multi server data transfer

--- src/app/socket.ts
+++ src/app/socket.ts
@@ -1,17 +1,37 @@
+// internal-imports
+import { redisPublisher, redisSubscriber } from '../core/index.js';
+
 // external-imports
 import { Server } from 'socket.io';
 
 // create a new Socket.IO server instance
 export const io = new Server();
 
+// subscribe to Redis channel for socket data
+redisSubscriber.subscribe('socket-data');
+
+// handle messages from Redis and emit to connected clients
+redisSubscriber.on('message', (_channel, message) => {
+  if (message) {
+    // parse the message from Redis
+    const { event, data } = JSON.parse(message);
+
+    // emit the event to all connected clients
+    io.emit(event, data);
+  }
+});
+
 // handle new client connections
 io.on('connection', socket => {
   // handle client-data event
-  socket.on('client-data', data => {
-    // log the received data along with the socket ID
-    console.log(`[${socket.id}] - ${data}`);
-
-    // broadcast the data to all other connected clients
-    io.emit('server-data', data);
+  socket.on('client-data', async data => {
+    // publish the data to Redis channel
+    await redisPublisher.publish(
+      'socket-data',
+      JSON.stringify({
+        event: 'server-data',
+        data,
+      })
+    );
   });
 });
